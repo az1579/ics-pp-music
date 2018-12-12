b2a3d6e0f332bcf182de393ab8b68901e47ce5d0
sw320888 <43874409+sw320888@users.noreply.github.com>
2018-12-12 15:23:31 +0800

    Update music.py

--- music.py
+++ music.py
@@ -1,14 +1,45 @@
+import pyaudio
+import wave
 
-import librosa
-import numpy as np
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 512
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "recordedFile.wav"
+device_index = 2
+audio = pyaudio.PyAudio()
 
+print("----------------------record device list---------------------")
+info = audio.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print ("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
-sr = 44100
+print("-------------------------------------------------------------")
 
-def update_wav(midi_object, my_name): #Done
-    # input: updated matrix
-    # updates the wav file for the current user, based on changes made by everybody
-    # returns updated matrix
-    synthesized_midi = midi_object.synthesize()
-    np_song = np.array(synthesized_midi)
-    librosa.output.write_wav('Music.wav', np_song, sr)
+index = int(input())
+print("recording via index "+str(index))
+
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,input_device_index = index,
+                frames_per_buffer=CHUNK)
+print ("recording started")
+Recordframes = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    Recordframes.append(data)
+print ("recording stopped")
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(Recordframes))
+waveFile.close()
